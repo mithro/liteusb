4de243ee39ab879978fffc048eb6abf1357ce823
Florent Kermarrec <florent@enjoy-digital.fr>
2015-09-07 12:30:47 +0200

    init repo

---
+++ liteusb/core/__init__.py
@@ -0,0 +1,31 @@
+from liteusb.common import *
+from liteusb.core.packet import LiteUSBPacketizer, LiteUSBDepacketizer
+from liteusb.core.crc import LiteUSBCRC32Inserter, LiteUSBCRC32Checker
+from liteusb.core.crossbar import LiteUSBCrossbar
+
+class LiteUSBCore(Module):
+    def __init__(self, phy, clk_freq, with_crc=True):
+        rx_pipeline = [phy]
+        tx_pipeline = [phy]
+
+        # depacketizer / packetizer
+        self.submodules.depacketizer = LiteUSBDepacketizer(clk_freq)
+        self.submodules.packetizer = LiteUSBPacketizer()
+        rx_pipeline += [self.depacketizer]
+        tx_pipeline += [self.packetizer]
+
+        if with_crc:
+            # crc checker / inserter
+            self.submodules.crc_rx = LiteUSBCRC32Checker()
+            self.submodules.crc_tx = LiteUSBCRC32Inserter()
+            rx_pipeline += [self.crc_rx]
+            tx_pipeline += [self.crc_tx]
+
+        # crossbar
+        self.submodules.crossbar = LiteUSBCrossbar()
+        rx_pipeline += [self.crossbar.master]
+        tx_pipeline += [self.crossbar.master]
+
+        # graph
+        self.submodules.rx_pipeline = Pipeline(*rx_pipeline)
+        self.submodules.tx_pipeline = Pipeline(*reversed(tx_pipeline))
---
+++ liteusb/frontend/wishbone.py
@@ -0,0 +1,9 @@
+from migen.fhdl.std import *
+
+from liteusb.common import *
+from misoclib.tools.wishbone import WishboneStreamingBridge
+
+class LiteUSBWishboneBridge(WishboneStreamingBridge):
+    def __init__(self, port, clk_freq):
+        WishboneStreamingBridge.__init__(self, port, clk_freq)
+        self.comb += port.sink.dst.eq(port.tag)
